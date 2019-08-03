4beb5fd78b96f0ee9f55f01236f9299ca3cb89d5
tsukasasuwa <53613868+tsukasasuwa@users.noreply.github.com>
2019-08-03 23:34:46 +0900

    2_C
    String Three Number

---
+++ 2_C.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+a,b,c=(int(x) for x in input().split())
+l=sorted([a,b,c])
+print(l[0],end="")
+print(" ",end="")
+print(l[1],end="")
+print(" ",end="")
+print(l[2],end="")
+
