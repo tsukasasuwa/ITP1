ab3a066eae7dafe2b5b4f6309cadfacadc52cfb4
tsukasasuwa <53613868+tsukasasuwa@users.noreply.github.com>
2019-08-08 10:55:37 +0900

    8_C

---
+++ 8_C.py
@@ -0,0 +1,113 @@
+
+# coding: utf-8
+
+# In[17]:
+
+
+xx=input()
+a=0
+b=0
+c=0
+d=0
+e=0
+f=0
+g=0
+h=0
+i=0
+j=0
+k=0
+l=0
+m=0
+n=0
+o=0
+p=0
+q=0
+r=0
+s=0
+t=0
+u=0
+v=0
+w=0
+x=0
+y=0
+z=0
+for ii in xx:
+    if ii=="A" or ii=="a":
+        a=a+1
+    elif ii=="B" or ii=="b":
+        b=b+1
+    elif ii=="C" or ii=="c":
+        c=c+1
+    elif ii=="D" or ii=="d":
+        d=d+1
+    elif ii=="E" or ii=="e":
+        e=e+1
+    elif ii=="F" or ii=="f":
+        f=f+1
+    elif ii=="G" or ii=="g":
+        g=g+1
+    elif ii=="H" or ii=="h":
+        h=h+1
+    elif ii=="I" or ii=="i":
+        i=i+1
+    elif ii=="J" or ii=="j":
+        j=j+1
+    elif ii=="K" or ii=="k":
+        k=k+1
+    elif ii=="L" or ii=="l":
+        l=l+1
+    elif ii=="M" or ii=="m":
+        m=m+1
+    elif ii=="N" or ii=="n":
+        n=n+1
+    elif ii=="O" or ii=="o":
+        o=o+1
+    elif ii=="P" or ii=="p":
+        p=p+1
+    elif ii=="Q" or ii=="q":
+        q=q+1
+    elif ii=="R" or ii=="r":
+        r=r+1
+    elif ii=="S" or ii=="s":
+        s=s+1
+    elif ii=="T" or ii=="t":
+        t=t+1
+    elif ii=="U" or ii=="u":
+        u=u+1
+    elif ii=="V" or ii=="v":
+        v=v+1
+    elif ii=="W" or ii=="w":
+        w=w+1
+    elif ii=="X" or ii=="x":
+        x=x+1
+    elif ii=="Y" or ii=="y":
+        y=y+1
+    elif ii=="Z" or ii=="z":
+        z=z+1
+print("a :",a)
+print("b :",b)
+print("c :",c)
+print("d :",d)
+print("e :",e)
+print("f :",f)
+print("g :",g)
+print("h :",h)
+print("i :",i)
+print("j :",j)
+print("k :",k)
+print("l :",l)
+print("m :",m)
+print("n :",n)
+print("o :",o)
+print("p :",p)
+print("q :",q)
+print("r :",r)
+print("s :",s)
+print("t :",t)
+print("u :",u)
+print("v :",v)
+print("w :",w)
+print("x :",x)
+print("y :",y)
+print("z :",z)
+
